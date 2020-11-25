cc3cb171344ffa3161059b67429366b7e1d3bd8f
pedro2479 <66635219+Pedro2479@users.noreply.github.com>
2020-11-25 18:53:41 +0100

    25/11/2020
    ejercicio1 de leaflet acabado

--- js/main2.js
+++ js/main2.js
@@ -1,15 +1,16 @@
 let map = L.map('map').setView([41.3868561, 2.1661102], 13);
+let marker = null;
+L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
-L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',{
- 
 }).addTo(map);
 
-map.on('click', function(ev) {
-  
-    let marker =L.marker([ev.latlng.lat,ev.latlng.lng])
-    .bindPopup(`" latitud :"+" "+"longitud :"`)
-    .addTo(map);
+map.on('click', function (e) {
 
-   
+    if (marker) map.removeLayer(marker)
+    let center = e.latlng;
+
+    marker = L.marker(center).bindPopup("Mis coordenadas son:<br><b>Lat: " + e.latlng.lat + " Lng:" + e.latlng.lng + "</b>").addTo(map);
+  
+    map.setView(center, 18);
 
 })
